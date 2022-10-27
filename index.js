8340c1bf1b59afb885ded8dc699011e9dfd6c10e
abdullahyahaya212@gmail.com <abdullahyahaya212@gmail.com>
2022-10-27 23:19:15 +0100

    finishing touch

--- index.js
+++ index.js
@@ -3,14 +3,7 @@ const server = express();
 const port = process.env.port || 4000;
 const fs = require("fs");
 
-//initialize express
-
-//index router
 server.get("/", (req, res) => {
-  res.send("Hello world");
-});
-
-server.get("/show_data", (req, res) => {
   fs.readFile(__dirname + "/" + "data.json", "utf8", (err, data) => {
     res.send(data);
   });
