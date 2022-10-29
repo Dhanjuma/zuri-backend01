bfcb6b12eb4920b7e560e8cbd0236fe7ccf60f71
abdullahyahaya212@gmail.com <abdullahyahaya212@gmail.com>
2022-10-29 15:11:54 +0100

    finished

--- index.js
+++ index.js
@@ -4,8 +4,11 @@ const port = process.env.port || 4000;
 const fs = require("fs");
 
 server.get("/", (req, res) => {
-  fs.readFile(__dirname + "/" + "data.json", "utf8", (err, data) => {
-    res.send(data);
+  res.send({
+    slackUsername: "dhanjuma",
+    backend: true,
+    age: 21,
+    bio: "I'm a student trying to boost my coding skills",
   });
 });
 
